6ad77d5707b37b14a96ce6805f75f5d8a2b4acbd
Rajaramanarb <rajaramanarb@gmail.com>
2024-12-28 12:45:53 +0530

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import localFont from "next/font/local";
 import React from "react";
+
+import { Toaster } from "@/components/ui/toaster";
+
+import { auth } from "@/auth";
 import "./globals.css";
-import Navbar from "@/components/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -26,26 +30,28 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth();
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
